ae12e2bd0d82c7d5153dbc9622f9b8127f127502
Lintahlo-001 <105794055+Lintahlo-001@users.noreply.github.com>
2024-10-26 02:31:20 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+
+function showModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = 'block';
+        document.body.classList.add("modal-open");
+    }
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = 'none';
+        document.body.classList.remove("modal-open");
+    }
+}
